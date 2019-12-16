8dd11b3ba2dad85ddb80be22ab1ee94c85e02f5b
AayushSabharwal <aayush.sabharwal@gmail.com>
2019-12-16 13:51:59 +0530

    added filter for 2009-01 to 2019-01

--- kdap_wikiArticlesEditedMonthwise.py
+++ kdap_wikiArticlesEditedMonthwise.py
@@ -12,10 +12,12 @@ def get_monthwise_articles_edited(template_name):
         revisions = get_revisions_of_article(article['title'])[article['title']]
         for revision in revisions:
             timestamp = revision['timestamp'][:7]
-            if timestamp not in monthwise_articles[template_name]:
-                monthwise_articles[template_name][timestamp] = []
-            if article not in monthwise_articles[template_name][timestamp]:
-                monthwise_articles[template_name][timestamp].append(article)
+            year = int(timestamp[:4])
+            if 2009 <= year < 2019:
+                if timestamp not in monthwise_articles[template_name]:
+                    monthwise_articles[template_name][timestamp] = []
+                if article not in monthwise_articles[template_name][timestamp]:
+                    monthwise_articles[template_name][timestamp].append(article)
     return monthwise_articles
 
 
