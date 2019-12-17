237441a7e89d2f6ae060b606cab01c9d3811b801
aayushsabharwal <aayush.sabharwal@gmail.com>
2019-12-17 21:39:14 +0530

    removed debug statements from graph file

--- kdap_wikiGraph.py
+++ kdap_wikiGraph.py
@@ -27,13 +27,7 @@ def get_graph(template_name):
     for timestamp in monthly_authors_count[template_name]:
         monthly_authors_count[template_name][timestamp] = len(monthly_authors_count[template_name][timestamp])
 
-    print(articles_edited_count[template_name])
     timestamps, y_articles = create_x_and_y(articles_edited_count, template_name)
-    print(timestamps)
-    print(y_articles)
-    print([(timestamps[i], y_articles[i]) for i in range(len(timestamps))])
-    sorted_keys = sorted(articles_edited_count[template_name].keys())
-    print([(sorted_keys[i], articles_edited_count[template_name][sorted_keys[i]]) for i in range(len(sorted_keys))])
     plt.plot(timestamps, y_articles, label='Articles')
 
     timestamps, y_articles = create_x_and_y(monthly_authors_count, template_name)
